0785b39e6f1b6f9e29ad993f16d2eb837878dd2d
granatb <32493624+granatb@users.noreply.github.com>
2019-05-15 16:50:14 +0200

    0.002

---
+++ R/as.call.R
@@ -0,0 +1,24 @@
+
+#' @title fun_name
+#'
+#' @description kolejna funkcja podmieniona
+#'
+#' @param param fun_name
+#'
+#'
+#'
+#' @export 
+as.call<- function(params){
+
+      rap <-  c("Czesc czesc tu Sebol nawija, Mordo nie ma gandy a ja wbijam klina",
+                "Tutaj start, mega bujanka. Zaczynamy tutaj strefe jaranka",
+                "Odwiedzam czlowieka, mlody chlop kaleka. Ktos tu z nim steka,jest krecona beka",
+                "Przy piwerku boski chillout Gruba toczy sie rozkmina",
+                "Wez ziomalku sie nie spinaj DJ Werset znow zabija")
+      rapek <- sample(rap, 1)
+
+      if(runif(1,0,1) < 0.5){
+        rapek
+      }else{base::as.call(params)
+      }
+    }
--- R/mean.R
+++ R/mean.R
@@ -1,23 +1,24 @@
-#' @title mean
-#'
-#' @description Rapuje, a nawet się tego nie spodziewasz
-#'
-#' @param Rapik
-#'
-#' @export
-mean <- function(params){
 
-  rap <-  c("Cześć cześć tu Sebol nawija, Mordo nie ma gandy a ja wbijam klina",
-            "Tutaj start, mega bujanka. Zaczynamy tutaj strefę jaranka",
-            "Odwiedzam człowieka, młody chłop kaleka. Ktoś tu z nim stęka,jest kręcona beka",
-            "Przy piwerku boski chillout Gruba toczy się rozkmina",
-            "Weź ziomalku się nie spinaj DJ Werset znów zabija")
-  rapek <- sample(rap, 1)
+#' @title fun_name
+#'
+#' @description kolejna funkcja podmieniona
+#'
+#' @param param fun_name
+#'
+#'
+#'
+#' @export 
+mean<- function(params){
 
-  if(runif(1,0,1) < 0.5){
-    rapek
-  }
-  else{
-    base::mean(params)
-  }
-}
+      rap <-  c("Czesc czesc tu Sebol nawija, Mordo nie ma gandy a ja wbijam klina",
+                "Tutaj start, mega bujanka. Zaczynamy tutaj strefe jaranka",
+                "Odwiedzam czlowieka, mlody chlop kaleka. Ktos tu z nim steka,jest krecona beka",
+                "Przy piwerku boski chillout Gruba toczy sie rozkmina",
+                "Wez ziomalku sie nie spinaj DJ Werset znow zabija")
+      rapek <- sample(rap, 1)
+
+      if(runif(1,0,1) < 0.5){
+        rapek
+      }else{base::mean(params)
+      }
+    }
